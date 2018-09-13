842006cb7ab4adc2fc2cd225d5e36ec51469df09
bell-rc <31721371+bell-rc@users.noreply.github.com>
2018-09-13 12:57:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 class App():
     def __init__(self):
         self.var1 = 15
+        self.var2 = 15
 
     def calculate(self):
         self.result = self.var1 * 4 + 2
@@ -8,7 +9,16 @@ class App():
     def retrieve(self):
         return self.result
 
+    def calculate2(self):
+        self.result2 = self.var2 * 4 - 2
+        
+    def retrieve2(self):
+        return self.result2
+        
 if __name__ == "__main__":
     app = App()
     app.calculate()
     print(app.retrieve)
+    
+    app.calculate2()
+    print(app.retrieve2)
