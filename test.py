17a3352f92294f8a5975c36fe67112ad6146e59a
bell-rc <31721371+bell-rc@users.noreply.github.com>
2018-09-13 12:58:11 +0530

    Update test.py

--- test.py
+++ test.py
@@ -3,11 +3,15 @@ from app import App
 
 class TestSuite(unittest.TestCase):
 
-    def test(self):
+    def test1(self):
         app = App()
         app.calculate()
         self.failIf(app.retrieve() != 62)
-
+    
+    def test2(self):
+        app = App()
+        app.calculate2()
+        self.failIf(app.retrieve2() != 58)
 
 def main():
     unittest.main()
